19c01986ef4549a45207cfe69d1d12500ffa4c1f
Ben Earlam <bearlam@thoughtworks.com>
2015-07-30 22:00:36 +0100

    Initialise repo

---
+++ test/roman_arithmetic/core_test.clj
@@ -0,0 +1,7 @@
+(ns roman-arithmetic.core-test
+  (:require [clojure.test :refer :all]
+            [roman-arithmetic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
