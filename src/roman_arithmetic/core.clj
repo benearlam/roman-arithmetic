02da40d6dcc46a24ee8d1e5071297e5611ce6e40
Ben Earlam <bearlam@thoughtworks.com>
2015-07-31 10:31:35 +0100

    Quick play with numeral addition in clojure

--- src/roman_arithmetic/core.clj
+++ src/roman_arithmetic/core.clj
@@ -1,6 +1,55 @@
-(ns roman-arithmetic.core)
+(ns roman-arithmetic.core
+  (:require [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def subtractive-forms
+  {:uncompact (array-map
+                "iv" "iiii"
+                "ix" "viiii")
+   :compact   (array-map
+                "viiiii" "x"
+                "iiiii" "v")})
+
+(def numeral-order
+  {"m" 1
+   "d" 2
+   "c" 3
+   "l" 4
+   "x" 5
+   "v" 6
+   "i" 7})
+
+(defn- compare-numeral
+  [left right]
+  (let [get-numeral-order #(get numeral-order (str %1))]
+    (<
+      (get-numeral-order left)
+      (get-numeral-order right))))
+
+(defn- apply-subtractive-forms
+  [numeral type]
+  (reduce
+    (fn [s [k v]]
+      (string/replace s k v))
+    numeral
+    (type subtractive-forms)))
+
+(defn- uncompact
+  [numeral]
+  (apply-subtractive-forms numeral :uncompact))
+
+(defn- compact
+  [numeral]
+  (apply-subtractive-forms numeral :compact))
+
+(defn- sort-left-to-right
+  [numeral]
+  (->> numeral
+       (sort (comp compare-numeral))
+       (apply str)))
+
+(defn add
+  [left right]
+  (->
+    (str (uncompact left) (uncompact right))
+    sort-left-to-right
+    compact))
--- test/roman_arithmetic/core_test.clj
+++ test/roman_arithmetic/core_test.clj
@@ -1,7 +1,23 @@
 (ns roman-arithmetic.core-test
   (:require [clojure.test :refer :all]
-            [roman-arithmetic.core :refer :all]))
+            [roman-arithmetic.core :refer :all]
+            [midje.sweet :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "`add`"
+       (fact "simple concat"
+         (add "i" "i") => "ii")
+
+       (fact "first subtractive"
+             (add "i" "iv") => "v")
+
+       (fact "next subtractive"
+             (add "i" "ix") => "x")
+
+       (fact "x is greater i and therefore sits to the left of it"
+             (add "i" "xi") => "xii")
+
+       (fact "c is greater v and therefore sits to the left of it"
+             (add "v" "c") => "cv")
+
+       (fact "m is greater v and therefore sits to the left of it"
+             (add "v" "m") => "mv"))
